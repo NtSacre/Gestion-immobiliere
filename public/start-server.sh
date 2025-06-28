12e2e75ac1ce0ed8773a544c5ea83a3a8c98af31
ronn-2002 <ondiapilironngeoffry@gmail.com>
2025-06-28 10:15:35 +0000

    Ajout de la fonctionnalité gestion agence

---
+++ public/start-server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+php -d display_er rors=1 -d display_startup_errors=1 -d error_reporting=E_ALL -S localhost:8080
+##le rendre excecutable avec ceci chmod +x start-server.sh ensuite lance ci sur le terminale
+
+## pour l'autoload
+##composer dump-autoload
